9f5bee254e7b419892e5cb74fd9efc4af76d3249
Kriish sharma <kriish.sharma2006@gmail.com>
2024-02-08 16:40:12 +0530

    Create addnode@end.c

---
+++ c/linked-list/addnode@end.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+void add(int x,struct node *head,struct node *add){
+     struct node *current = head;
+     while (current->next != NULL) { /* current != NULL will lead to segmentaion fault The issue causing a segmentation fault is that after the while loop, when current becomes NULL, you're attempting to access current->next, which leads to dereferencing a NULL pointer, resulting in undefined behavior and, in this case, a segmentation fault.*/
+        current = current->next; // Move to the next node
+    }
+    current->next = add;
+    add -> data = x;
+    add -> next = NULL;
+    
+}
+
+void view(struct node *head) {
+    struct node *current = head;
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next; // Move to the next node
+    }
+    printf("\n");
+}
+
+int main() {
+    // initializing nodes :
+    struct node head;
+    struct node *first;
+    struct node *second;
+    struct node *third;
+    struct node *forth;
+
+    // allocating memory :
+    first = (struct node*) malloc(sizeof(struct node));
+    second = (struct node*) malloc(sizeof(struct node));
+    third = (struct node*) malloc(sizeof(struct node));
+    forth = (struct node*) malloc(sizeof(struct node));
+
+    // data assign 
+    head.data = 0; // creating a dummy 
+    head.next = first;
+
+    first->data = 10; // x->y = (*x).y
+    first->next = second; 
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = NULL;
+// travesing and adding node at en of list 
+    view(head.next);
+    int data4;
+    printf("\nTYPE DATA TO ADD : \n");
+    scanf("%d",&data4);
+    add(data4,head.next,forth);
+    view(head.next);
+    
+
+    // Free allocated memory
+    free(first);
+    free(second);
+    free(third);
+    free(forth);
+    
+    
+    // Avoiding dangling pointers
+    first = NULL;
+    second = NULL;
+    third = NULL;
+    forth = NULL;
+    head.next = NULL;
+    return 0;
+}
