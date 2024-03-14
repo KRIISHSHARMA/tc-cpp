38267670e19ddcce13ff594b98dcb14a9ecdbd59
Kriish sharma <kriish.sharma2006@gmail.com>
2024-03-14 09:56:22 +0530

    merge

---
+++ college/5.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+
+void merging(int arr1[] , int arr2[] , int total , int merged[]){
+	for (int i = 0 ; i < total ; i++){
+		merged[i] = arr1[i];
+	}
+
+	for (int i = 0 ; i < total ; i++){
+		merged[total+i] = arr2[i];
+	}
+}
+
+
+int main(){
+	int total;
+	int arr1[total];
+	int arr2[total];
+	
+	cout<<"Enter Number of Elements for arr : ";
+	cin>>total;
+	cout<<endl;
+	cout<<"Enter Elements for arr1\n";
+	for (int i = 0 ; i < total ; i++){
+	       cout<<"arr["<<i<<"] =";
+           cin>>arr1[i];
+           cout<<endl;
+        }
+	cout<<endl;
+	cout<<"Enter Elements for arr2\n";
+	for (int i = 0 ; i < total ; i++){
+	       cout<<"arr["<<i<<"] =";
+           cin>>arr2[i];
+           cout<<endl;
+        }
+        
+    int merged[total*2];
+    merging(arr1,arr2,total,merged);
+    cout<<"AFTER MERGING : "<<endl;
+    for (int i = 0 ; i < (total*2) ; i++){
+	    cout<<merged[i]<<endl; 
+    }
+
+
+	return 0;
+}
+
